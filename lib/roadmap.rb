d51f8cd575baea05209928059ad4aef6f7c0b74e
Michael Davis <mdavis93@hotmail.com>
2017-11-26 14:19:43 -0600

    Added Checkpoint Submission

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -8,4 +8,16 @@ module Roadmap
     response = self.class.get("/checkpoints/#{id}", headers: { authorization: @auth_token })
     JSON.parse(response.body)
   end
+
+  def create_submission(checkpoint, branch, commit_link, comment)
+    response = self.class.post('/checkpoint_submissions', headers: {authorization: @auth_token}, body:{
+      assignment_branch: branch,
+      assignment_commit_link: commit_link,
+      checkpoint_id: checkpoint,
+      comment: comment,
+      enrollment_id: get_me['current_enrollment']['id']
+      })
+      
+      JSON.parse(response.body)
+  end
 end
