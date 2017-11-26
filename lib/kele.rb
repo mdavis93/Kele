1cbb1c392589460894934d5f4464a336b7b42dd1
Michael Davis <mdavis93@hotmail.com>
2017-11-26 11:50:11 -0600

    Checkpoint 3: COMPLETE

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
@@ -9,5 +10,10 @@ class Kele
     @auth_token = post_response['auth_token']
 
     raise "Invalid Login Credentials" if @auth_token.nil?
+  end
+
+  def get_me
+    response = self.class.get("/users/me", headers: {authorization: @auth_token} )
+    JSON.parse(response.body)
   end
 end
