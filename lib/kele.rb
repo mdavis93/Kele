6220bcfae26019587ef2cdb55fdcc964fa6625c8
Michael Davis <mdavis93@hotmail.com>
2017-11-26 11:19:53 -0600

    Checkpoint 2: COMPLETE

---
+++ lib/kele.rb
@@ -0,0 +1,13 @@
+require 'httparty'
+
+class Kele
+  include HTTParty
+  base_uri "https://www.bloc.io/api/v1"
+
+  def initialize(email, password)
+    post_response = self.class.post("/sessions", body: {email: email, password: password})
+    @auth_token = post_response['auth_token']
+
+    raise "Invalid Login Credentials" if @auth_token.nil?
+  end
+end
